923d961fd4125a99ea5daa42caadd295877e89d5
fisher <774977731@qq.com>
2020-01-15 16:39:37 +0800

    完成用户协议

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,13 @@
-const url = 'https://tkx.spacemans.cn';
-// const url = "https://ttkx.spacemans.cn";
+// const url = 'https://tkx.spacemans.cn';
+const url = "https://ttkx.spacemans.cn";
 
 module.exports = {
     publicPath:'./',
     // baseUrl: '/',
     devServer: {
         proxy: {
-            '/bigData': { target: url,changeOrigin: true }
+            '/bigData': { target: url,changeOrigin: true },
+            '/getUserAgreement': { target: url,changeOrigin: true },
         }
     }
 };
