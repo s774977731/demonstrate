957eb654676efda9ea36d1e7e43c94cd4c4fb439
fisher <774977731@qq.com>
2020-01-16 11:23:27 +0800

    兼容低版本手机

--- public/invite_page/js/register.js
+++ public/invite_page/js/register.js
@@ -10,11 +10,11 @@ new Vue({
         isToasting:false
     },
     mounted:function() {
-
+        
     },
     methods:{
       //获取验证码
-      getCode() {
+      getCode:function() {
         if(!this.isPhoneNumber(this.phone)) {
             this.showToast('请输入正确的手机号');
             return false;
@@ -38,27 +38,28 @@ new Vue({
         });
       },
       //倒计时
-      cutDown() {
-        let second = this.second;
-        let timeout = setTimeout(() => {
+      cutDown:function() {
+        var _this = this;
+        var second = this.second;
+        var timeout = setTimeout(function() {
           if(second <= 0) {
             clearTimeout(timeout);
-            this.text = '获取验证码';
-            this.second = 60;
+            _this.text = '获取验证码';
+            _this.second = 60;
           }else {
             second -= 1;
-            this.text = '重新获取' + second +'s';
-            this.second = second;
-            this.cutDown();
+            _this.text = '重新获取' + second +'s';
+            _this.second = second;
+            _this.cutDown();
           }
         },1000)
       },
       //进入用户协议
-      enterUserAgreement() {
+      enterUserAgreement:function() {
         window.location.href = './agreement.html'
       },
       //注册
-      handleRegister() {
+      handleRegister:function() {
           var _this = this;
           if(!this.phone) {
               this.showToast('请输入手机号');
@@ -88,7 +89,7 @@ new Vue({
           });
       },
       //隐藏弹框
-      hideModal() {
+      hideModal:function() {
         this.isRegister = false;
       },
       //判断是否是手机号
@@ -97,24 +98,26 @@ new Vue({
         return reg.test(tel);
       },
       //显示toast
-      showToast(msg) {
-          this.toastText = msg;
-          this.isToasting = true;
-          setTimeout(() => {
-              this.isToasting = false;
+      showToast:function(msg) {
+          var _this = this;
+          _this.toastText = msg;
+          _this.isToasting = true;
+          setTimeout(function() {
+              _this.isToasting = false;
           }, 2000);
       },
       //获取invite_id
-      getQueryString(key) {
-        let search = window.location.search.replace(/^\?/, "");
-        let pairs = search.split("&");
-        let paramsMap = pairs.map(pair => {
-            let [key, value] = pair.split("=");
+      getQueryString:function(key) {
+        var _this = this;
+        var search = window.location.search.replace(/^\?/, "");
+        var pairs = search.split("&");
+        var paramsMap = pairs.map(function(pair) {
+            var [key, value] = pair.split("=");
             return [decodeURIComponent(key), decodeURIComponent(value)];
-        }).reduce((res, [key, value]) => Object.assign(res, { [key]: value }), {});
+        }).reduce(function(res, [key, value]) { return Object.assign(res, { [key]: value })}, {});
         return paramsMap[key] || "";
       },
-      params_aes(params) {
+      params_aes:function(params) {
           var PUBLIC_KEY = '-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAo6MMnrY7f5NOxSutybU0JZCd42st/OvaMmQoX3AhAWyuktc+urKpYq0fJROzKiN59QXRNvqllUVD0P068PDltqqA9uQAy8BqlS0kFTw8ZtbsEgKYxmOCaWQ6ljXkdDT82iAHT2tKqYqP+Ws2DfVO+uHIFl442udHc9huuWz+xu+qviyD4VlmViW0dxkdzofwl6vPXOG0AI3+Xmnto43md1OfcVAgozUb4f7pCT9ZY3nNRSuYm4zVHNg0iFcZ37s3b/VfsbVOXgKGBvPfzKIOnNEdAgnM4ObxKfOztjEfV6AoGsPPOHXr28nM+hkry7EFGTNe0rRnQyscjeqpj7zjSwIDAQAB-----END PUBLIC KEY-----';
           var encrypt_rsa = new RSA.RSAKey();
           encrypt_rsa = RSA.KEYUTIL.getKey(PUBLIC_KEY);
@@ -123,18 +126,18 @@ new Vue({
           console.log("加密结果：" + encStr);
           return encStr;
       },
-      header_md5_aes_base64(params) {
+      header_md5_aes_base64:function(params) {
           if(typeof params == 'object') params = JSON.stringify(params);
           params = '!sp' + params + 'mans!';
-          let md5Str = md5(params);
+          var md5Str = md5(params);
           console.log("md5加密结果：" + md5Str);
-          const PUBLIC_KEY = '-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAo6MMnrY7f5NOxSutybU0JZCd42st/OvaMmQoX3AhAWyuktc+urKpYq0fJROzKiN59QXRNvqllUVD0P068PDltqqA9uQAy8BqlS0kFTw8ZtbsEgKYxmOCaWQ6ljXkdDT82iAHT2tKqYqP+Ws2DfVO+uHIFl442udHc9huuWz+xu+qviyD4VlmViW0dxkdzofwl6vPXOG0AI3+Xmnto43md1OfcVAgozUb4f7pCT9ZY3nNRSuYm4zVHNg0iFcZ37s3b/VfsbVOXgKGBvPfzKIOnNEdAgnM4ObxKfOztjEfV6AoGsPPOHXr28nM+hkry7EFGTNe0rRnQyscjeqpj7zjSwIDAQAB-----END PUBLIC KEY-----';
+          var PUBLIC_KEY = '-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAo6MMnrY7f5NOxSutybU0JZCd42st/OvaMmQoX3AhAWyuktc+urKpYq0fJROzKiN59QXRNvqllUVD0P068PDltqqA9uQAy8BqlS0kFTw8ZtbsEgKYxmOCaWQ6ljXkdDT82iAHT2tKqYqP+Ws2DfVO+uHIFl442udHc9huuWz+xu+qviyD4VlmViW0dxkdzofwl6vPXOG0AI3+Xmnto43md1OfcVAgozUb4f7pCT9ZY3nNRSuYm4zVHNg0iFcZ37s3b/VfsbVOXgKGBvPfzKIOnNEdAgnM4ObxKfOztjEfV6AoGsPPOHXr28nM+hkry7EFGTNe0rRnQyscjeqpj7zjSwIDAQAB-----END PUBLIC KEY-----';
           var encrypt_rsa = new RSA.RSAKey();
           encrypt_rsa = RSA.KEYUTIL.getKey(PUBLIC_KEY);
           var encStr = encrypt_rsa.encrypt(md5Str);
           encStr = RSA.hex2b64(encStr);
           console.log("md5 rsa加密结果：" + encStr);
-          // let base64Str = Base64.encode(encStr);
+          // var base64Str = Base64.encode(encStr);
           // console.log("md5 rsa base64加密结果：" + base64Str);
           return encStr;
       }
